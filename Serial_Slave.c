d188de4e06ea6a2dfb6982cbbcdc0a2f4ec4277c
Anan-Khalid <anan8137@gmail.com>
2024-09-02 19:01:51 +0300

    Update Serial_Slave.c

--- Serial_Slave.c
+++ Serial_Slave.c
@@ -1,1 +1,11 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String dataFromMaster = Serial.readStringUntil('\n');
+    Serial.println("Received from Master: " + dataFromMaster);
+  }
+}
 
