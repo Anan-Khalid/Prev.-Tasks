83897a3f529b6a28077f1f81b1c74b11070dae5c
Anan-Khalid <anan8137@gmail.com>
2024-09-02 18:58:40 +0300

    Update I2C_Slave.c

--- I2C_Slave.c
+++ I2C_Slave.c
@@ -1,1 +1,16 @@
+#include <Wire.h>
 
+const int slaveAddress = 8;
+
+void setup() {
+  Wire.begin(slaveAddress);
+  Wire.onRequest(requestEvent); 
+}
+
+void loop() {
+}
+
+void requestEvent() {
+  // Send data to master
+  Wire.write("Hello from I2C slave!");
+}
